867cdb1b9567e48e7400b012039a1c2dae250fca
Furkan <fantastisk-4@hotmail.com>
2022-08-07 13:39:23 +0200

    Simpole react app

--- server/server.js
+++ server/server.js
@@ -2,14 +2,16 @@ import express from "express";
 
 const app = express();
 
-app.get("/login", (req,res, next) => {
-    res.json({username: "Noen Andre Personer"});
-})
+app.get("/login", (req, res, next) => {
+  res.json({ username: "Noen Andre Personer" });
+});
 
-app.post("/login", (reg,res,next) => {
-    res.sendStatus(401);
-})
+app.post("/login", (reg, res, next) => {
+  res.sendStatus(401);
+});
+
+app.use(express.static("public"))
 
 const server = app.listen(process.env.PORT || 3000, () => {
-    console.log(`started on http://localhost:${server.address().port}`);
+  console.log(`started on http://localhost:${server.address().port}`);
 });
